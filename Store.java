93235c3387d356469642b5fce1426d92fe924480
iulian <iulian@iulian-hplaptop15dw1xxx>
2022-08-31 10:10:43 +0300

    initial test commit

---
+++ Store.java
@@ -0,0 +1,14 @@
+package com.example.restservice;
+
+public class Store {
+
+	private String name;
+	
+	public Store(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
